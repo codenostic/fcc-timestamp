7840b7735fd5bcda0ffea5454ef9235ff6d797d0
Glitch (fcc-timestamp-ms-bhupesh) <none>
2018-03-21 16:36:54 +0000

    Exporting to gitbhupesh/fcc-timestamp

--- public/date.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(dateString){
-  var dateObject = {unix: 1, natural: 1};
-  var date = new Date();
-  date.setSeconds(dateString);
-  return date;
-}
--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 var date = require("./public/date.js");
+var moment = require('moment');
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -15,10 +17,34 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-app.get("/*", function(request, response, next){
-  var data = request.params["0"];
-  var dateObject = date(data);
-  response.send(dateObject);
+
+app.get("/:date", function(req, res){
+  var jsonResult = {unix: 0, natural: ''}
+	// check date is valid or not 
+	var mNatural = moment(req.params.date, ['MMMM DD, YYYY', 'MMM D, YYYY', 'MMMM D, YYYY'],true);
+	var mUnix = moment(req.params.date, ['x'], true);
+	
+	if(!(mNatural.isValid() || mUnix.isValid())){
+		jsonResult.unix = null;
+		jsonResult.natural = null;
+		res.send(JSON.stringify(jsonResult))
+		return;
+	}
+	
+	// check date is sting format if yes - send response 
+	if(mNatural.isValid()){
+		jsonResult.unix = mNatural.format('x');
+		jsonResult.natural = mNatural.format('MMMM DD, YYYY');
+		res.send(JSON.stringify(jsonResult));
+		return;
+	}
+	// check if date is unix format if yes send response 
+	if(mUnix.isValid()){
+		jsonResult.unix = mUnix.format('x');
+		jsonResult.natural = mUnix.format('MMMM DD, YYYY');
+		res.send(JSON.stringify(jsonResult));
+		return;
+  }
 });
         
 // listen for requests :)
