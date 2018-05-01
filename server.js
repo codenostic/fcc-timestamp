8177dcbc325274f9c6bac25f2e36c5d3e716808d
Glitch (fcc-timestamp-ms-bhupesh) <none>
2018-05-01 11:31:29 +0000

    dont need express.static

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var moment = require('moment');
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
